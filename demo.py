64c57d9b7634c7693778c83da34971800bd12370
Stefan van der Walt <stefan@sun.ac.za>
2013-04-11 17:49:20 +0200

    Better demo output visualization.

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
 from _growcut import growcut
 import numpy as np
-from skimage import io
+from skimage import io, img_as_float
 
-image = io.imread('sharkfin_small.jpg')
+image = img_as_float(io.imread('sharkfin_small.jpg'))
 state = np.zeros((image.shape[0], image.shape[1], 2))
 
 foreground_pixels = np.array([(90, 90), (50, 90)])
@@ -18,7 +18,7 @@ out = growcut(image, state, window_size=5, max_iter=500)
 
 import matplotlib.pyplot as plt
 
-f, (ax0, ax1) = plt.subplots(1, 2)
+f, (ax0, ax1) = plt.subplots(1, 2, figsize=(7, 3))
 
 ax0.imshow(image, interpolation='nearest', cmap=plt.cm.gray)
 ax0.plot(foreground_pixels[:, 1], foreground_pixels[:, 0],
@@ -28,8 +28,10 @@ ax0.plot(background_pixels[:, 1], background_pixels[:, 0],
 ax0.set_title('Input image')
 ax0.axis('image')
 
-ax1.imshow(out, interpolation='nearest', cmap=plt.cm.gray, vmin=0, vmax=1)
+ax1.imshow(out[..., None] * image, interpolation='nearest', cmap=plt.cm.gray, vmin=0, vmax=1)
 ax1.set_title('Foreground / background')
 ax0.axis('image')
+
+plt.savefig('demo.png')
 
 plt.show()
