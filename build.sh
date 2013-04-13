8d2421017dd3cfa2db89d4d022e04a44307b05f0
Aron Ahmadia <aron@ahmadia.net>
2013-04-13 16:32:47 +0100

    in-place builds

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 numpy_dir=/usr/local/lib/python2.7/site-packages/numpy
-python setup.py build_ext --include-dirs=$numpy_dir/core/include
-cmd="PYTHONPATH=.:./build/lib.macosx-10.8-x86_64-2.7 python tests/test_growcut_py.py"
+python setup.py build_ext --inplace --include-dirs=$numpy_dir/core/include
+cmd="PYTHONPATH=. python tests/test_growcut_py.py"
 echo running $cmd
 eval $cmd
